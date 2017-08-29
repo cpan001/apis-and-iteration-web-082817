04efe431253da86e8626dcafc9f4d434723880b2
Ashe Austaire <AsheAustaire@gmail.com>
2017-08-29 14:11:49 -0400

    version 1

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,33 +1,60 @@
 require 'rest-client'
 require 'json'
 require 'pry'
+require_relative './command_line_interface'
+
+
+
+def api_request(urls)
+
+    urls.inject([]){ |array, url|
+
+        api_string = RestClient.get(url)
+
+        final_url = JSON.parse(api_string)
+
+        array << final_url
+
+    }
+
+end
+
+def film_helper(character_hash, character)
+
+
+    character_hash["results"].each { |char|
+        return char['films'] if char["name"].downcase == character
+
+    }
+
+end
+
 
 def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
+
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+
+    char_movies = film_helper(character_hash, character)
+
+    api_request(char_movies)
+
 end
 
+
 def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+
+    films_hash.each_with_index do |film, num|
+
+        puts "#{num + 1} #{film["title"]}"
+
+    end
 end
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
   parse_character_movies(films_hash)
 end
-
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
